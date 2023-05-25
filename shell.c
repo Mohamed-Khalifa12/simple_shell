d1264af0f248e1b058f5b2db2609faa15f4afc3b
Mohamed Khalifa <mohamed.khalifa01@eng-st.cu.edu.eg>
2023-05-25 12:37:56 +0300

    ..

--- shell.c
+++ shell.c
@@ -1,89 +1,59 @@
 #include "shell.h"
 
 /**
- * sig_handler - checks if Ctrl C is pressed
- * @sig_num: int
+ * main - Entry point to program
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: Returns condition
  */
-void sig_handler(int sig_num)
-{
-	if (sig_num == SIGINT)
-	{
-		_puts("\n#cisfun$ ");
-	}
-}
 
-/**
-* _EOF - handles the End of File
-* @len: return value of getline function
-* @buff: buffer
- */
-void _EOF(int len, char *buff)
+int main(__attribute__((unused)) int argc, char **argv)
 {
-	(void)buff;
-	if (len == -1)
+	char *input, **cmd, **commands;
+	int count = 0, i, condition = 1, stat = 0;
+
+	if (argv[1] != NULL)
+		read_file(argv[1], argv);
+	signal(SIGINT, signal_to_handle);
+
+	while (condition)
 	{
+		count++;
 		if (isatty(STDIN_FILENO))
+			prompt();
+		input = _getline();
+		if (input[0] == '\0')
+			continue;
+		history(input);
+		commands = separator(input);
+		for (i = 0; commands[i] != NULL; i++)
 		{
-			_puts("\n");
-			free(buff);
-		}
-		exit(0);
-	}
-}
-/**
-  * _isatty - verif if terminal
-  */
-
-void _isatty(void)
-{
-	if (isatty(STDIN_FILENO))
-		_puts("#cisfun$ ");
-}
-/**
- * main - Shell
- * Return: 0 on success
- */
-
-int main(void)
-{
-	ssize_t len = 0;
-	char *buff = NULL, *value, *pathname, **arv;
-	size_t size = 0;
-	list_path *head = '\0';
-	void (*f)(char **);
-
-	signal(SIGINT, sig_handler);
-	while (len != EOF)
-	{
-		_isatty();
-		len = getline(&buff, &size, stdin);
-		_EOF(len, buff);
-		arv = splitstring(buff, " \n");
-		if (!arv || !arv[0])
-			execute(arv);
-		else
-		{
-			value = _getenv("PATH");
-			head = linkpath(value);
-			pathname = _which(arv[0], head);
-			f = checkbuild(arv);
-			if (f)
+			cmd = parse_cmd(commands[i]);
+			if (_strcmp(cmd[0], "exit") == 0)
 			{
-				free(buff);
-				f(arv);
+				free(commands);
+				exit_bul(cmd, input, argv, count, stat);
 			}
-			else if (!pathname)
-				execute(arv);
-			else if (pathname)
+			else if (check_builtin(cmd) == 0)
 			{
-				free(arv[0]);
-				arv[0] = pathname;
-				execute(arv);
+				stat = handle_builtin(cmd, stat);
+				free(cmd);
+				continue;
 			}
+			else
+			{
+				stat = check_cmd(cmd, input, count, argv);
+			}
+			/*if (commands[i + 1] == NULL)
+			{
+				free(commands);
+				break;
+			}*/
+			free(cmd);
 		}
+		free(input);
+		free(commands);
+		wait(&stat);
 	}
-	free_list(head);
-	freearv(arv);
-	free(buff);
-	return (0);
+	return (stat);
 }
